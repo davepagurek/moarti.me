5220e86d2ae46d6aa51b63e7439f2b22ce844740
Bilal Akhtar <me@itsbilal.com>
2014-11-01 19:39:48 -0400

    REST endpoint to get an event

--- routes/events.js
+++ routes/events.js
@@ -1,3 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var Event = mongoose.model("Event");
 
+router.get('/event/:id', function(req, res){
+	var id = req.params.id;
+
+	Event.findById(id, function(err, theEvent){
+		var isAttendee = theEvent.attendees.some(function(attendeeId){
+			return attendeeId.equals(req.user._id);
+		});
+
+		if (!isAttendee) {
+			res.status(401).send({error: "Unauthorized"});
+			return;
+		}
+
+		res.send({"event":theEvent});
+	});
+});
