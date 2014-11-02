4ed5dc091eb1cfed3d1be0bebbe16728ba4e1442
Dave Pagurek <dave@pahgawks.com>
2014-11-01 22:52:35 -0400

    Event admin page

---
+++ public/js/eventAdmin.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", function() {
+  var public = document.getElementById("link");
+  link.value = document.URL;
+  link.addEventListener("click", function() {
+    link.select();
+  });
+});
