f2548fd9f0cf949dabd569b4fa0fcc4cc816808e
Bilal Akhtar <me@itsbilal.com>
2014-11-01 20:16:05 -0400

    Create new event REST endpoint in events.js, add dependency on Q

--- routes/events.js
+++ routes/events.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 var Event = mongoose.model("Event");
+var User = mongoose.model("User");
+var Q = require("q");
 
 router.get('/event/:id', function(req, res){
 	var id = req.params.id;
@@ -16,6 +18,44 @@ router.get('/event/:id', function(req, res){
 			return;
 		}
 
-		res.send({"event":theEvent});
+		var promises = theEvent.attendees.map(function(attendeeId){
+			return Q.ninvoke(User.findById(attendeeId, "displayName"), "exec");
+		});
+
+		Q.all(promises)
+			.then(function(attendees){
+				theEvent.attendees = attendees;
+				res.send({"event":theEvent});
+			})
+			.done();
 	});
 });
+
+router.post('/event/new', function(req, res){
+	var userId = req.user._id;
+	var title = req.body.title;
+	var start = new Date(req.body.start);
+	var end = new Date(start.getTime() + 24*60*60*1000);
+
+	var theEvent = new Event({
+		user: userId,
+		title: title,
+		start: start,
+		end: end
+	});
+
+	theEvent.save(function(err){
+		if (!err) {
+			res.send({success: 1});
+		}
+	});
+});
+
+router.post('/event/:id/addCalendar', function(req, res){
+	
+});
+
+router.post('/event/:id/calculate', function(req, res){
+
+});
+
