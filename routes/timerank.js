5e48a662e4b5d6a3c61ba3050ce96baa6412da73
Bilal Akhtar <me@itsbilal.com>
2014-11-02 11:48:26 -0500

    Temporary fix for timeRank

--- routes/timerank.js
+++ routes/timerank.js
@@ -209,7 +209,7 @@ function timeRank (n,people,pref) {
 	});
 
 	top = []; //[intervals[0].interval,intervals[1].interval,intervals[2].interval]];
-	for (var i = 0; i < intervals.length || top.length < 3; i++) {
+	for (var i = 0; i < intervals.length && top.length < 3; i++) {
 		var temp = intervals[i].interval;
 		var hi = inInterval(pref,temp.endTime);
 		var low = inInterval(pref,temp.startTime);
